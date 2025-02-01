2d940270e5fd84b62612467dceb81ed0c51606d8
rishab <rishabpradhan15@gmail.com>
2025-02-01 22:07:34 +0545

    authorization of dashboard

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+    const isAuthenticated = localStorage.getItem("token"); // Check if token exists
+
+    return isAuthenticated ? children : <Navigate to="/login" replace />;
+}
