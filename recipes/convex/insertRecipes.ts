c887e4f8fd56d101311540c900864f7f67b4a6cb
rishab <rishabpradhan15@gmail.com>
2025-05-02 16:53:55 +0545

    insertion changes

--- recipes/convex/insertRecipes.ts
+++ recipes/convex/insertRecipes.ts
@@ -1,18 +1,21 @@
-import { mutation } from "./_generated/server"; //
+import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const insertRecipes = mutation({
-    args: {
-        title: v.string(),
-        image: v.string(),
-        cookingTime: v.number(),
-        instructions: v.array(v.string()),
-        calories: v.number(),
-    },
-    handler: async (ctx, args) => {
-        return await ctx.db.insert("recipes", {
-            ...args,
-            createdAt: Date.now(),
-        });
-    },
+  args: {
+    title: v.string(),
+    image: v.string(),
+    cookingTime: v.float64(),
+    calories: v.float64(),
+    instructions: v.array(v.string()),
+    ingredients: v.array(v.string()),
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const createdAt = Date.now();
+    await ctx.db.insert("recipes", {
+      ...args,
+      createdAt,
+    });
+  },
 });
