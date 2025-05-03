fd6058aa0a240b2d4231fb218c7b4ef72fdfe112
rishab <rishabpradhan15@gmail.com>
2025-05-03 11:11:22 +0545

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav.jsx";
 import Dispaly1 from "./components/Dispaly1.jsx";
@@ -9,39 +7,54 @@ import FormData from "./components/FormData.jsx";
 import LoginPage from "./components/LoginPage.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
-
+import RecipeList from "./components/RecipeList.jsx";
+import AdminProtectedRoute from "./components/AdminProtectedRoute.jsx";
+import AdminLogin from "./components/AdminLogin.jsx";
+import AdminPage from "./components/AdminPage.jsx";
 function App() {
-
-
   return (
     <>
-       <Router>
-
-           {/* default pages*/}
-           <Routes>
-               <Route path="/" element={
-                   <>
-                       <Nav/>
-                       <Dispaly1 />
-                       <Display2/>
-                       <Recipe/>
-                   </>
-               }/>
-               {/* form*/}
-               <Route path="form" element={<FormData/>}/>
-               <Route path="login" element={<LoginPage/>}/>
-               {/* protected routes*/}
-               <Route path="dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
-
-
-
-
-           </Routes>
-
-       </Router>
-
+      <Router>
+        {/* default pages*/}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Nav />
+                <Dispaly1 />
+                <Display2 />
+                <Recipe />
+              </>
+            }
+          />
+          {/* form*/}
+          <Route path="recipes" element={<RecipeList />} />
+          <Route path="form" element={<FormData />} />
+          <Route path="login" element={<LoginPage />} />
+          {/* protected routes*/}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* admin routes routes*/}
+          <Route path="/AdminLogin" element={<AdminLogin />} />
+          <Route
+            path="/AdminPage"
+            element={
+              <AdminProtectedRoute>
+                <AdminPage />
+              </AdminProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
